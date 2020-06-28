b5a03478715962d6953a72faed4c833060288ea8
zoie <zoietady@gmail.com>
2020-06-29 09:41:42 +1000

    Initial Commit

---
+++ Sammy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sammy
+//
+//  Created by Zoie Tad-y on 6/29/20.
+//  Copyright © 2020 Zoie Tad-y. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
